88371ee6056e227323a460b4ea8273ba54a192f9
Aiperi Anarbekovna <ajperikamcybekova146@gmail.com>
2024-02-22 15:51:26 +0600

    input

--- main.js
+++ main.js
@@ -44,3 +44,19 @@ function mergeObjects() {
 console.log(mergeObjects({type: 'bass', string: 4}, {type: 'ukulele'}));
 console.log(mergeObjects({name: 'John', age: 42}, {type: 'car', name: 'ferrari'}, {type: 'bike'}));
 console.log(mergeObjects());
+document.getElementById('inputField').addEventListener('input', function() {
+    const inputText = this.value;
+    const itemsArray = inputText.split(',').map(item => item.trim()).filter(item => item !== '');
+    const outputDiv = document.getElementById('outputDiv');
+    outputDiv.innerHTML = '';
+
+    if (itemsArray.length > 0) {
+        const olElement = document.createElement('ol');
+        itemsArray.forEach(item => {
+            const liElement = document.createElement('li');
+            liElement.textContent = item;
+            olElement.appendChild(liElement);
+        });
+        outputDiv.appendChild(olElement);
+    }
+});
